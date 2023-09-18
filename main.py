24627dd0225ceb8fe516398cc45563197bb2eff3
v <viniciuswolves@gmail.com>
2023-09-17 21:32:45 -0300

    feat: gera um excel com informacoes obrigatorias

--- main.py
+++ main.py
@@ -84,14 +84,71 @@ def get_categories_fee():
     print("Pressione ENTER para sair")
 
 
+def get_categories_requirements():
+    data = []
+
+    df = pd.read_excel("result.xlsx")
+
+    column = df["ID Categoria"]
+
+    categories = column.values
+
+    variations = 0
+    custom_fields = 0
+    gtin = False
+
+    for category in categories:
+        url = f"https://api.mercadolibre.com/categories/{category}/attributes"
+        response = requests.get(url)
+        category_raw = response.json()
+
+        variations_list = []
+        custom_fields_list = []
+
+        for i in category_raw:
+            try:
+                if i["tags"]["allow_variations"]:
+                    variations_list.append(i["name"])
+                    variations += 1
+            except KeyError:
+                continue
+
+        for i in category_raw:
+            try:
+                if i["tags"]["required"]:
+                    custom_fields_list.append(i["name"])
+                    custom_fields += 1
+            except KeyError:
+                continue
+
+    # for i in category_raw:
+    #     try:
+    #         if i["tags"]["conditional_required"]:
+    #             print(f"[>] GTIN Obrigatório")
+    #             gtin = True
+    #     except KeyError:
+    #         continue
+
+        data.append({'ID Categoria': category, 'Var1': variations_list[0] if len(variations_list) > 0 else "N/A",
+                     'Var2': variations_list[1] if len(variations_list) > 1 else "N/A",
+                     'CF1': custom_fields_list[0] if len(custom_fields_list) > 0 else "N/A",
+                     'CF2': custom_fields_list[1] if len(custom_fields_list) > 1 else "N/A"})
+
+    df = pd.DataFrame(data)
+    df.to_excel("categorias_campos.xlsx", index=False, engine="openpyxl")
+    print("Excel gerado.")
+    print("Pressione ENTER para sair")
+
+
 if __name__ == "__main__":
-    options = [1, 2, 3]
+    options = [1, 2, 3, 4]
     option = 0
     while option not in options:
         option = int(input(
             "[1] Refazer a planilha com todos os IDs\n"
             "[2] Refazer a planilha com todas as informaçoes (usa a opçao 1 como base)\n"
             "[3] Ambos (Recomendado mensalmente)\n"
+            "[4] Programando...\n"
             "R: "))
 
     if option == 1:
@@ -101,3 +158,5 @@ if __name__ == "__main__":
     if option == 3:
         get_categories()
         get_categories_fee()
+    if option == 4:
+        get_categories_requirements()
